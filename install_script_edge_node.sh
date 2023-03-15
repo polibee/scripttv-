3697648fc9b55ab0ed86fab3b8a205027fbc203f
polyooe <lhb293425@gmail.com>
2023-03-15 11:33:42 +0800

    Update install_script_edge_node.sh

--- install_script_edge_node.sh
+++ install_script_edge_node.sh
@@ -4,7 +4,9 @@
 # Update and install required packages
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install build-essential git tmux -y
+sudo apt-get install build-essential -y 
+sudo apt-get install git -y
+sudo apt install tmux
 
 # Install Go
 wget https://go.dev/dl/go1.19.2.linux-amd64.tar.gz 
@@ -23,17 +25,3 @@ sudo cp -r ./integration/scriptnet ../scriptnet
 sudo chmod -R 777 /usr/local/go 
 git config --global --add safe.directory /usr/local/go/src/github.com/scripttoken/script 
 make install
-
-# Start Script nodes using tmux
-tmux new -s node '/usr/local/go/bin/script start --config=../scriptnet/node'
-tmux new -s cli '/usr/local/go/bin/scriptcli daemon start --port=16889'
-tmux new -s detail '/usr/local/go/bin/scriptcli query lightning'
-
-# Open wallet.script.tv in browser and stake SCPT
-echo "Open wallet.script.tv in your browser and follow the instructions to stake SCPT."
-
-# Download and install Edge Node application
-echo "Navigate to token.script.tv/download in your browser and download the Linux version of the Edge Node application."
-echo "Follow the setup guide instructions and open the application once installed."
-echo "Click Activate Wallet, enter the required node address and summary details, check the T&C box and proceed."
-echo "Toggle the button to On to activate the Edge Node. The transcoding jobs will start and you will be earning rewards."
